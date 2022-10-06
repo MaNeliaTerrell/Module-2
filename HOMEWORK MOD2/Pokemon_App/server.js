018cf2e6c3d27b5dd96552ba58e0d376d7ca1dab
MaNeliaTerrell <nterrell1014@gmail.com>
2022-10-06 17:26:13 -0400

    Connected my database, can see jason in the browser.

--- HOMEWORK MOD2/Pokemon_App/server.js
+++ HOMEWORK MOD2/Pokemon_App/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const pokemon = require('./Models/pokemon')
 
 const app = express();
 const PORT = 3000
@@ -8,6 +9,10 @@ app.get('/', function(req, res) {
     res.send('Welcome to the Pokemon App')
 })
 
+app.get('/pokemon', function(req, res) {
+    console.log(req.params);
+    res.send('pokemon')
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
